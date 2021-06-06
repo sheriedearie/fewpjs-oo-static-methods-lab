0e70056585b1768737ea310d7d16b61d839ea9c4
Sheridan <sheridanmaustad@gmail.com>
2021-06-06 00:23:57 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 class Formatter {
-  //add static methods here
+  static capitalize (string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  static sanitize (str){
+    
+  }
+
+  static titleize(){
+
+  }
 }
