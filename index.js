722773d0956d8b1d3ba00015a614e521359837c4
Sheridan <sheridanmaustad@gmail.com>
2021-06-06 07:33:22 -0600

    Cwork and progress

--- index.js
+++ index.js
@@ -7,6 +7,16 @@ class Formatter {
   }
 
   static titleize(text){
-   return text.replace("the, a, an, but, of, and, for, at, by,from", '').toLowerCase();
+    const splitText = text.split(" ")
+    console.log(splitText)
+  
+    // gain access to each word
+    // iterating over the word
+    // need to keep track of prohibited words list
+    // compare each word to see if it is in the prohibited words list
+    // if word isn't in "list" caps it. if in list don't caps it
+    // include word in string that will return at the end- regardless
+    // make sure the first word is always caps- even if on the "list"
+    // forEach.map
   }
 }
