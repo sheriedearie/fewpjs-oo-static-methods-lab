17d4b541b7c5430966a7772f263e52564bcf936a
Sheridan <sheridanmaustad@gmail.com>
2021-06-06 09:24:05 -0600

    Work and progress

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 class Formatter {
-  static capitalize (string){
+  static capitalize(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
-  static sanitize (str){
-   return str.replace(/[^A-Za-z0-9 '-]/g, '') ;
+  static sanitize(str) {
+    return str.replace(/[^A-Za-z0-9 '-]/g, '');
   }
 
-  static titleize(text){
+  static titleize(text) {
     const splitText = text.split(" ")
-    console.log(splitText)
-  
-    // gain access to each word
-    // iterating over the word
-    // need to keep track of prohibited words list
-    // compare each word to see if it is in the prohibited words list
-    // if word isn't in "list" caps it. if in list don't caps it
-    // include word in string that will return at the end- regardless
-    // make sure the first word is always caps- even if on the "list"
-    // forEach.map
+    const prohibitedList = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    const newArray = []
+    splitText.forEach((el) => {
+      if (splitText[0] = el){
+        return text;
+      }
+      else if (!prohibitedList.includes(el)) {
+        newArray.push(el[0].toUpperCase() + el.slice(1))
+      } else {
+        newArray.push(el)
+      }
+    }) 
+    return newArray.join(" ");
   }
 }
